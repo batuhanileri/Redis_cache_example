37d8491622da554eb8c6bb0e1db8e903b221750e
batuhanileri <bileriofficial@gmail.com>
2025-01-08 01:53:19 +0300

    IMemoryCache DI and set get method added

--- InMemory.Caching/Controllers/ValuesController.cs
+++ InMemory.Caching/Controllers/ValuesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
+using System.Security.Cryptography.X509Certificates;
 
 namespace InMemory.Caching.Controllers
 {
@@ -13,6 +14,23 @@ namespace InMemory.Caching.Controllers
         public ValuesController(IMemoryCache memoryCache)
         {
             _memoryCache = memoryCache;
+        }
+
+        [HttpGet("set/{name}")]
+        public void Set(string name)
+        {
+            _memoryCache.Set("name",name);
+
+        }
+        [HttpGet]
+        public string Get()
+        {
+            if(_memoryCache.TryGetValue<string>("name",out string name))
+            {
+                return name.Substring(3);
+            }
+             
+            return "";
         }
 
 
