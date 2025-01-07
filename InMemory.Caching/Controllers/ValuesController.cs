dde467c7e3fdb8fefbf7437ccc2b9ab63c3ce680
batuhanileri <bileriofficial@gmail.com>
2025-01-08 02:02:23 +0300

    AbsoluteExpiration,SlidingExpiration

--- InMemory.Caching/Controllers/ValuesController.cs
+++ InMemory.Caching/Controllers/ValuesController.cs
@@ -33,7 +33,21 @@ namespace InMemory.Caching.Controllers
             return "";
         }
 
+        [HttpGet("setDate")]
+        public void SetDate()
+        {
+            _memoryCache.Set<DateTime>("date", DateTime.Now, options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddMinutes(1),
+                SlidingExpiration  = TimeSpan.FromSeconds(5)
+            });
+        }
 
+        [HttpGet("getDate")]
+        public DateTime Date()
+        {
+            return _memoryCache.Get<DateTime>("date");
+        }
 
     }
 }
